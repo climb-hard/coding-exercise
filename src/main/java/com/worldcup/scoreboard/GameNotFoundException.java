f1be245e16fe76d715e1a516be5d8bf0560cab09
climbhard <climbhard6@gmail.com>
2025-07-09 22:53:39 +0200

    feat: Second step - all requirements and tests for edge cases

--- src/main/java/com/worldcup/scoreboard/GameNotFoundException.java
+++ src/main/java/com/worldcup/scoreboard/GameNotFoundException.java
@@ -5,4 +5,4 @@ public class GameNotFoundException extends RuntimeException {
     public GameNotFoundException(String homeTeam, String awayTeam) {
         super(String.format("Game between %s and %s not found", homeTeam, awayTeam));
     }
-}
+} 
